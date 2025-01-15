51beb2c86959f2787060784bb53b0a72a0d96255
Sara <sarasaini3116@gmail.com>
2025-01-15 07:14:42 +0530

    Learnt about Variables

---
+++ javascript/01-variables-datatypes/02-variables.js
@@ -0,0 +1,23 @@
+let age = 24;
+console.log(age); // 24
+
+let name = "Tony Stark";
+console.log(name); // Tony Stark
+const price=100.50;
+console.log(price); // 100.50
+let x=null;
+let y;
+console.log(x); // null
+console.log(y); // undefined
+
+const isFollow = false;
+console.log(isFollow); // false
+//variables name should be meaningful
+//Dinamically typed language
+//variables are case sensitive
+//variables can be reassigned
+
+
+//always define variables using keywords
+//var, let, const
+
