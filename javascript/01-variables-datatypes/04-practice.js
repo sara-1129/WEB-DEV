b024c4e84bd6ebd152cdb59487d896d1c39b9dca
Sara <sarasaini3116@gmail.com>
2025-01-15 08:03:58 +0530

    Practiced Questions

---
+++ javascript/01-variables-datatypes/04-practice.js
@@ -0,0 +1,13 @@
+const product = {
+    name:"Parker Jotter",
+    type:"Ballpoint Pen",
+    rating:4,
+    offerPrice:270,
+    actualPrice:285,
+    deal:true,
+    color:"Black",
+    bodyMaterial:"metallic",
+    site:"Amazon",
+
+};
+//with reference to apna college
---
+++ javascript/01-variables-datatypes/05-practice.js
@@ -0,0 +1,8 @@
+const profile ={
+    name:"Shradha Khapra",
+    posts:195,
+    followers:"569k",
+    following:4,
+    positon:"Co Founder Apna College",
+    profession:"Educator",
+};
